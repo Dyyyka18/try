693527b56bde8f824b181e289a1da5a3e0b62b48
Dyyyka18 <dyyyka8@gmail.com>
2024-12-23 21:56:07 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const infoButton = document.getElementById("infoButton"); // Tombol utama
+
+    // Event listener untuk membuka link
+    infoButton.addEventListener("click", () => {
+        window.location.href = "https://drive.google.com/file/d/1QpCHddu1IlQ1tRml3i4Rna7W4I2yCx7Q/view?usp=sharing"; // Ganti dengan URL tujuan
+    });
+});
